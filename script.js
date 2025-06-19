d96fa0b0a1d0ac00f398ce6801a0aea7d2c436de
Shreyansh <149056401+justtshreyansh@users.noreply.github.com>
2025-06-19 16:42:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
+const a = document.querySelector('input[name="fname"]');
+const b = document.querySelector('input[name="lname"]');
 function getFormvalue() {
+	let c = a.value.trim();
+	let d = b.value.trim();
+	if(c==='' || d===''){
+		alert("Please provide valid input");
+		return ;
+	}
+
+	alert(`${c} ${d}`)
+
+	
     //Write your code here
 
 }
